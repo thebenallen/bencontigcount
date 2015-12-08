00f722d1b96776fa6392ef6d50d03f714e5ed6cb
thebenallen <allenbh@ornl.gov>
2015-12-08 14:17:17 -0500

    first commit

---
+++ bencontigcount.spec
@@ -0,0 +1,9 @@
+/*
+A KBase module: bencontigcount
+*/
+
+module bencontigcount {
+	/*
+	Insert your typespec information here.
+	*/
+};
---
+++ test/bencontigcount_server_test.py
@@ -0,0 +1,64 @@
+import unittest
+import os
+import json
+import time
+
+from os import environ
+from ConfigParser import ConfigParser
+from pprint import pprint
+
+from biokbase.workspace.client import Workspace as workspaceService
+from bencontigcount.bencontigcountImpl import bencontigcount
+
+
+class bencontigcountTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        token = environ.get('KB_AUTH_TOKEN', None)
+        cls.ctx = {'token': token}
+        config_file = environ.get('KB_DEPLOYMENT_CONFIG', None)
+        cls.cfg = {}
+        config = ConfigParser()
+        config.read(config_file)
+        for nameval in config.items('bencontigcount'):
+            cls.cfg[nameval[0]] = nameval[1]
+        cls.wsURL = cls.cfg['workspace-url']
+        cls.wsClient = workspaceService(cls.wsURL, token=token)
+        cls.serviceImpl = bencontigcount(cls.cfg)
+
+    @classmethod
+    def tearDownClass(cls):
+        if hasattr(cls, 'wsName'):
+            cls.wsClient.delete_workspace({'workspace': cls.wsName})
+            print('Test workspace was deleted')
+
+    def getWsClient(self):
+        return self.__class__.wsClient
+
+    def getWsName(self):
+        if hasattr(self.__class__, 'wsName'):
+            return self.__class__.wsName
+        suffix = int(time.time() * 1000)
+        wsName = "test_bencontigcount_" + str(suffix)
+        ret = self.getWsClient().create_workspace({'workspace': wsName})
+        self.__class__.wsName = wsName
+        return wsName
+
+    def getImpl(self):
+        return self.__class__.serviceImpl
+
+    def getContext(self):
+        return self.__class__.ctx
+
+    def test_your_method(self):
+        # Prepare test objects in workspace if needed using 
+        # self.getWsClient().save_objects({'workspace': self.getWsName(), 'objects': []})
+        #
+        # Run your method by
+        # ret = self.getImpl().your_method(self.getContext(), parameters...)
+        #
+        # Check returned data with
+        # self.assertEqual(ret[...], ...) or other unittest methods
+        pass
+        
